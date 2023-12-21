67907aab311645a5f8bef041fef9fcd06be95253
Beriatan <michalbrenda@outlook.com>
2023-12-21 06:22:33 +0000

    Modified arc lengths

--- raaComputerGraphicsAssignment1/Config.cpp
+++ raaComputerGraphicsAssignment1/Config.cpp
@@ -11,8 +11,8 @@ void initConfig() {
 	g_Config.m_fKineticEnergyThreshold = 40000000000000.0f;
 	g_Config.m_fNodeSpacingInCube = 150.0f;
 	g_Config.m_fNodeSpacingInLine = 50.0f;
-	g_Config.m_fArcLength = 100.0f;
-	g_Config.m_fMaxArcLength = 500.0f;
+	g_Config.m_fArcLength = 200.0f;
+	g_Config.m_fMaxArcLength = 600.0f;
 
 }
 
